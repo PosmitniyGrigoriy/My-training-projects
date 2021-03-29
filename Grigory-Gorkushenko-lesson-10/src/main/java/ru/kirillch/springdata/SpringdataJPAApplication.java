ba1a96961b3fbc1da348ff100a142b0876f3325b
Grigoriy <pga.profile@gmail.com>
2021-03-29 19:36:59 +1000

    1 project divided into 2 projects and bugs fixed

---
+++ Grigory-Gorkushenko-lesson-10/src/main/java/ru/kirillch/springdata/SpringdataJPAApplication.java
@@ -0,0 +1,36 @@
+package ru.kirillch.springdata;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.kirillch.springdata.entity.Person;
+import ru.kirillch.springdata.repository.PersonRepository;
+
+@SpringBootApplication
+public class SpringdataJPAApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataJPAApplication.class, args);
+    }
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Person anna = new Person("Anna", "Andreeva", "Samara");
+        Person olesya = new Person("Olesya", "Andreeva", "Moscow");
+        Person alex = new Person("Alex", "Andreev", "Omsk");
+
+        List<Person> people = Arrays.asList(anna, olesya, alex);
+
+        personRepository.saveAll(people);
+
+        System.out.println("<<<<<<<<" + personRepository.findAll());
+    }
+}
--- Grigory-Gorkushenko-lesson-10/src/test/java/ru/kirillch/springdata/repository/PersonRepositoryTest.java
+++ Grigory-Gorkushenko-lesson-10/src/test/java/ru/kirillch/springdata/repository/PersonRepositoryTest.java
@@ -5,10 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.kirillch.springdata.entity.Person2;
-
+import ru.kirillch.springdata.entity.Person;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -21,7 +19,7 @@ public class PersonRepositoryTest {
     @Test
     public void findByFirstName() throws Exception {
 
-        List<Person2> people = personRepository.findByFirstName("Anna");
+        List<Person> people = personRepository.findByFirstName("Anna");
 
         assertThat(people).hasSize(1);
         assertThat(people.get(0).getLastName()).isEqualTo("Andreeva");
@@ -30,7 +28,7 @@ public class PersonRepositoryTest {
 
     @Test
     public void findAllOrderedByFirstName() throws Exception {
-        List<Person2> people = personRepository.findAllOrderedByFirstName();
+        List<Person> people = personRepository.findAllOrderedByFirstName();
 
         assertThat(people).hasSize(3);
         assertThat(people.get(2).getFirstName()).isEqualTo("Olesya");
